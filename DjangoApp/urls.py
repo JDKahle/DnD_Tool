7ee6b5d7b420bb24f061684fbd6afa7918944af5
JDKahle <112479319+JDKahle@users.noreply.github.com>
2022-10-06 22:45:45 +0200

    Update urls.py

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('people/', include('people.urls'))
+    path('people/', include('people.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
